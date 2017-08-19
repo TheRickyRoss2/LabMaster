db828409b12b2adc26ee35467501fd61d7797c32
Ric Rodriguez <rirrodri@ucsc.edu>
2017-08-19 00:52:59 -0700

    update

--- adap.py
+++ adap.py
@@ -12,8 +12,17 @@ class Keithley_2400(object):
         return 1
     def set_voltage(self, vout):
         return 100
+    def configure_sweep(self, start_volt, end_volt, step_volt, delay_time, hold_time):
+        return 90
+    
+class Agilent_E4980A(object):
+    def init(self, gpib):
+        return 50
+    def set_voltage(self):
+        return 74
 
 if __name__=="__main__":
     device = Keithley_2400()
     print(device.init(24))
     print(device.set_voltage(1000))
+    
