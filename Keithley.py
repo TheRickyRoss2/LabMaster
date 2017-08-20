5c8acb9f9a5aae48c26310fda03f0ab5aee000c3
Ric <Ric@Ric-PC>
2017-08-20 15:47:54 -0700

    added keithley functions

--- Keithley.py
+++ Keithley.py
@@ -15,26 +15,46 @@ class Keithley_2400(object):
                 self.inst = rm.open_resource(x)
                 
         try:
-            print(self.inst.query("*IDN?"))
+            print(self.inst.query("*IDN?;"))
         finally:
             print "Unable to open visa resource"
-        self.inst.query("*RST")
+        self.inst.query("*RST;")
         self.inst.query("*ESE 1;*SRE 32;*CLS;:FUNC:CONC ON;:FUNC:ALL;:TRAC:FEED:CONT NEV;:RES:MODE MAN;")
-        print self.inst.query(":SYST:ERR?")
+        print self.inst.query(":SYST:ERR?;")
         
         
-    def measure_function(self, x):
+    def get_function(self, x):
+        return{
+            0:":VOLT",
+            1:":CURR",
+            2:":RES"
+        }.get(x, "CURR")
+        
+    def get_source_mode(self, x):
         return{
             0:"VOLT",
-            1:"CURR",
-            2:"RES"
-        }.get(x, "CURR")
+            1:"CURR"
+        }.get(x, "VOLT")
         
     def configure_measurement(self, funct=0):
         
         assert(funct>=0 and funct <3), "Invalid function specified"
+        self.inst.query(self.get_function(funct)+"RANG:AUTO ON;")
+    
+    def configure_source(self, source_mode, output_level, compliance):
+        assert(source_mode>=0 and source_mode<2), "Invalid Source function"
+        assert(output_level>-1100 and output_level < 1100), "Voltage out of range"
+        assert(compliance<0.5), "Compliance out of range"
+        func = self.get_source_mode(source_mode)
+        self.inst.query("SOUR:FUNC "+func+";:SOUR:"+func+" "+str(float(output_level))+";:CURR:PROT "+str(float(compliance))+";")
         
+    def enable_output(self, output):
+        if output is True:
+            self.inst.query("OUTP ON;")
+            return
+        self.inst.query("OUTP OFF;")
         
+    def read_single_point(self, timeout = 10000):
         
         
     def set_voltage(self, vout):
