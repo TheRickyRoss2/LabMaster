ebd6c4ae96f5824e6d59ef32c80a48d06881d2b4
Ric Rodriguez <rirrodri@ucsc.edu>
2017-08-22 01:39:50 -0700

    Added spa interface

--- Agilent.py
+++ Agilent.py
@@ -1,6 +1,80 @@
 import visa
 import time
 
+class Agilent4156(object):
+    
+    def init(self, gpib=6):
+        """Set up gpib controller for device"""
+        
+        assert(gpib >= 0), "Please enter a valid GPIB address"
+        self.gpib_addr = gpib
+        
+        print "Initializing Agilent semiconductor parameter analyzer"
+        rm = visa.ResourceManager()
+        self.inst = rm.open_resource(rm.list_resources()[0])
+        for x in rm.list_resources():
+            if str(self.gpib_addr) in x:
+                print "found"
+                self.inst = rm.open_resource(x)
+                
+        print self.inst.query("*IDN?")
+        
+        self.inst.write("*RST")
+        self.inst.write("*ESE 60;*SRE 48;*CLS;")
+        self.inst.timeout= 10000
+        
+    def configure_measurement(self, _mode=1):
+        mode = {0:":PAGE:CHAN:MODE SWE;", 1:":PAGE:CHAN:MODE SAMP", 2:":PAGE:CHAN:MODE QSCV"}.get(
+             _mode,":PAGE:CHAN:MODE SAMP")
+         
+        self.inst.write(mode)
+        
+    def configure_sampling_measurement(self, _mode = 0, _filter = False, auto_time=True, 
+                                       hold_time=0, interval=4e-3, total_time=0.4,no_samples=101):
+        mode = {0:"LIN", 1:"L10", 2:"L25", 3:"L50", 4:"THIN"}.get(_mode, "LIN")
+        self.inst.write(":PAGE:MEAS:SAMP:MODE " +mode+";")
+        self.inst.write(":PAGE:MEAS:SAMP:HTIM "+str(hold_time)+";")
+        self.inst.write(":PAGE:MEAS:SAMP:IINT "+str(interval)+";")
+        if _filter is True:
+            self.inst.write(":PAGE:MEAS:SAMP:FILT ON;")
+        else:
+            self.inst.write(":PAGE:MEAS:SAMP:FILT OFF;")
+            
+        self.inst.write(":PAGE:MEAS:SAMP:PER "+str(total_time)+";")
+        if auto_time is True:
+            self.inst.write(":PAGE:MEAS:SAMP:PER:AUTO ON;")
+        else:
+            self.inst.write(":PAGE:MEAS:SAMP:PER:AUTO OFF;")
+            
+        self.inst.write(":PAGE:MEAS:SAMP:POIN "+str(no_samples)+";")
+        
+        
+    def configure_sampling_stop(self, stop_condition = False, no_events=1,
+                                 _event_type=0, delay=0, thresh=0, var ="V2"):
+        
+        event_type = {0:"LOW", 1:"HIGH", 2:"ABSL", 3:"ABSH"}.get(_event_type, "LOW")
+        if stop_condition is True:
+            self.inst.write(":PAGE:MEAS:SAMP:SCON ON;")
+        else:
+            self.inst.write(":PAGE:MEAS:SAMP:SCON OFF;")
+            
+        self.inst.write(":PAGE:MEAS:SAMP:SCON:ECO "+str(no_events)+";")
+        self.inst.write(":PAGE:MEAS:SAMP:SCON:EDEL "+str(delay)+";")
+        self.inst.write(":PAGE:MEAS:SAMP:SCON "+event_type+";")
+        self.inst.write(":PAGE:MEAS:SAMP:SCON:NAME \'"+var+"\';")
+        self.inst.write(":PAGE:MEAS:SAMP:SCON:THR "+str(thresh)+";")
+        
+    def measurement_actions(self, _action=2):
+        
+        action = {0:"APP", 1:"REP", 2:"SING", 3:"STOP"}.get(_action,"SING")
+        self.inst.write(":PAGE:SCON:"+action+";")
+        
+    def wait_for_acquisition(self):
+        return self.inst.query("*OPC?")
+        
+    def read_trace_data(self, var="I1"):
+        return self.inst.query(":FORM:BORD NORM;DATA REAL, 64;:DATA? \'"+var+"\';")
+        
 class AgilentE4980a(object):
     
     def init(self, gpib=19):
