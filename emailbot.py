943db40c174b66982fe6a1f62dff4bacb8a47f9f
Ric Rodriguez <rirrodri@ucsc.edu>
2017-12-25 00:18:46 -0800

    Refactored for readability

--- emailbot.py
+++ emailbot.py
@@ -1,25 +1,52 @@
 import smtplib
-import mimetypes
 import email
 import email.mime.application
 
-def sendMail(filename, recipients):
+EMAIL_USERNAME = "adapbot@gmail.com"
+EMAIL_PASSWORD = "AdapBot143"
+SMTP_SERVER = "smtp.gmail.com:587"
+
+
+def send_mail(attached_file_name, recipients):
+    """
+    This is an helper function for sending the data to your email
+    :param attached_file_name: file to send (full path)
+    :param recipients: comma separated list of email recipients
+    :return: None
+    """
+    email_message = email.mime.Multipart.MIMEMultipart()
+    email_message['Subject'] = 'Experiment Data'
+    email_message['From'] = EMAIL_USERNAME
+    email_message['To'] = ", ".join(recipients)
     
-    msg = email.mime.Multipart.MIMEMultipart()
-    msg['Subject'] = 'Experiment Data'
-    msg['From'] = 'adapbot@gmail.com'
-    msg['To'] = ", ".join(recipients)
-    
-    body = email.mime.Text.MIMEText("""Your experiment has finished! Here is the data.
-    """)
-    msg.attach(body)
-    fp = open(filename, 'rb')
-    att = email.mime.application.MIMEApplication(fp.read(), _subtype="xlsx")
-    fp.close()
-    att.add_header('Content-Disposition', 'attachment', filename=filename)
-    msg.attach(att)
-    s = smtplib.SMTP('smtp.gmail.com:587')
-    s.starttls()
-    s.login('adapbot@gmail.com', 'AdapBot143')
-    s.sendmail('adapbot@gmail.com', recipients, msg.as_string())
-    s.quit()
+    body = email.mime.Text.MIMEText("""Your experiment has 
+    finished!Here is the data.""")
+    email_message.attach(body)
+
+    file_reader = open(attached_file_name, 'rb')
+    attachment = email.mime.application.MIMEApplication(
+        file_reader.read(),
+        _subtype="xlsx"
+    )
+
+    file_reader.close()
+
+    attachment.add_header(
+        'Content-Disposition',
+        'attachment',
+        filename=attached_file_name
+    )
+
+    email_message.attach(attachment)
+
+    send_email_connection = smtplib.SMTP(SMTP_SERVER)
+    send_email_connection.starttls()
+    send_email_connection.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+
+    send_email_connection.sendmail(
+        EMAIL_USERNAME,
+        recipients,
+        email_message.as_string()
+    )
+
+    send_email_connection.quit()
